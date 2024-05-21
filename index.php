a426400a3288f28d33461c265b2af32db2a99390
230038 <230038tj@yse-c.net>
2024-05-21 09:51:27 +0900

    ボタン枠追加

--- index.php
+++ index.php
@@ -14,26 +14,25 @@
         <div class="calculator bg-white rounded p-8 shadow-md">
             <form action="update.php" method="post">
                 <input type="text" id="display" name="price" class="w-full mb-4 px-2 py-1 border rounded" readonly>
-                <button class="btn">計上</button>
+                <button class="btn border w-12 h-8 mb-4">計上</button>
             </form>
             <div class="grid grid-cols-4 gap-4">
-                
-                <button class="btn" onclick="addToDisplay('7')">7</button>
-                <button class="btn" onclick="addToDisplay('8')">8</button>
-                <button class="btn" onclick="addToDisplay('9')">9</button>
-                <button class="btn" onclick="clearAll()">AC</button>
-                <button class="btn" onclick="addToDisplay('4')">4</button>
-                <button class="btn" onclick="addToDisplay('5')">5</button>
-                <button class="btn" onclick="addToDisplay('6')">6</button>
-                <button class="btn" onclick="calculate('+')">+</button>
-                <button class="btn" onclick="addToDisplay('1')">1</button>
-                <button class="btn" onclick="addToDisplay('2')">2</button>
-                <button class="btn" onclick="addToDisplay('3')">3</button>
-                <button class="btn" onclick="calculate('*')">x</button>
-                <button class="btn" onclick="addToDisplay('0')">0</button>
-                <button class="btn" onclick="calculateTax()">Tax</button>
-                <a class="btn" href="sales/">売上</a>
-                <button class="btn" onclick="calculateTotal()">=</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('7')">7</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('8')">8</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('9')">9</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="clearAll()">AC</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('4')">4</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('5')">5</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('6')">6</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="calculate('+')">+</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('1')">1</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('2')">2</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('3')">3</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="calculate('*')">x</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="addToDisplay('0')">0</button>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="calculateTax()">Tax</button>
+                <a class="btn border w-12 h-8 flex justify-center items-center" href="sales/">売上</a>
+                <button class="btn border w-12 h-8 flex justify-center items-center" onclick="calculateTotal()">=</button>
             </div>
     </div>
             <!-- <div class="mt-5">
